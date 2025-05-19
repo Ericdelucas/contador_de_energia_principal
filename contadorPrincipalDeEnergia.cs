e6de068f7dabd6d1e649d7e046674ea0f7c3e441
ERIC DE LUCAS SILVA <126726641+Ericdelucas@users.noreply.github.com>
2025-05-19 19:43:47 -0300

    Update contadorPrincipalDeEnergia.cs

--- contadorPrincipalDeEnergia.cs
+++ contadorPrincipalDeEnergia.cs
@@ -1,40 +1,87 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
-public class ContadorEnergua : MonoBehaviour
-
+public class EnergyUIManager : MonoBehaviour
 {
+    [Header("Referências de UI")]
+    [SerializeField] GameObject menuPanel;       // Arraste aqui o MenuPanel
+    [SerializeField] GameObject energyPanel;     // Arraste aqui o EnergyPanel
+    [SerializeField] GameObject energyText;            // Arraste aqui o TxtEnergyValue
+    [SerializeField] Button EnergyButton;
+    int qualObj;
+    public List<GameObject> construcoes = new List<GameObject>();// Arraste aqui o EnergyButton
 
-    
-    public int energia;
-    public GameObject contador;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int energia = 0;
+
+    private void Awake()
+    {
+        EnergyButton.onClick.AddListener(AddEnergy);
+    }
     void Start()
     {
-        contador = GameObject.FindGameObjectWithTag("ContadorEnergia");
-       
+        qualObj = -1;
+        // Garante que o painel de energia comece oculto
+        menuPanel.SetActive(true);
+        energyPanel.SetActive(false);
+      
     }
 
-    // Update is called once per frame
-    public void AddEnergia(int adicao)
+    // Chamado pelo BtnOpenEnergy.OnClick()
+    public void ShowEnergyScreen()
     {
-        if (energia < 10000000)
+        menuPanel.SetActive(false);
+        energyPanel.SetActive(true);
+    }
+
+    // Chamado pelo BtnCloseEnergy.OnClick()
+    public void HideEnergyScreen()
+    {
+        energyPanel.SetActive(false);
+        menuPanel.SetActive(true);
+    }
+
+    // Chamado pelo BtnAddEnergy.OnClick()
+    public void AddEnergy()
+    {
+        if (energia < 1000)
         {
-            energia += adicao;
+            energyText.GetComponent<ContadorEnergua>().AddEnergia(3);
+            
         }
     }
-    private void Update()
+
+   public void Construir()
     {
-        contador.GetComponent<TMP_Text>().text = "Energia:" + energia;
+        if (energyText.GetComponent<ContadorEnergua>().energia >= 300)
+        {
+            qualObj++;
+            energyText.GetComponent<ContadorEnergua>().AddEnergia(-300);
+            construcoes[qualObj].gameObject.SetActive(true);
+        }
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Left mouse button
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("EnergyGiver")) // Or use tag, or compare the actual GameObject
+                {
+                    AddEnergy();
+                }
+            }
+        }
         if (energia < 0)
         {
             SceneManager.LoadSceneAsync(2);
         }
-    }
-    public void RemoveEnergia(int valor)
-    {
-        energia = energia - valor;
     }
 
 }
